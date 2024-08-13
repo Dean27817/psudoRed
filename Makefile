1e80aae177189b70c9f30490f41b48187965b6af
Dean27817 <dean@amjas.com>
2024-08-13 17:51:06 -0600

    Added Corewar Compiler

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 compile:
 	gcc -o prod src/main.c -lyaml
 run:
-	./prod source.txt output.txt
+	./prod source.txt output.red
+	rm prod
+	./src/asm output.red
+	rm output.red
+	./src/corewar output.
--- src/main.c
+++ src/main.c
@@ -3,20 +3,38 @@
 #include <assert.h>
 #include <stdbool.h>
 
-bool openFile(FILE *file, char *fileName, char *fileType)
+class subCommand
 {
-	file = fopen(fileName, fileType);
+
+};
+
+class command
+{
+	public:
+
+
+	private:
+
+	void createSubCommand(int subNum, )
+	{
+		for(int i = 0; i < subNum; i++)
+		{
+
+		}
+	}
+};
+
+bool openFile(FILE **file, char *fileName, char *fileType)
+{
+	*file = fopen(fileName, fileType);
 
 	if(file != NULL)
 	{
 		printf("found %s file\n", fileName);
 		return true;
 	}
-	else
-	{
-		printf("couldent find %s file", fileName);
-		return false;
-	}
+	printf("couldent find %s file", fileName);
+	return false;
 }
 
 
@@ -38,17 +56,17 @@ int main(int argc, char* argv[])
 	FILE* COMMANDS;
 
 	//loads the files into memory
-	if(!openFile(IN, argv[1], "r"))
+	if(!openFile(&IN, argv[1], "r"))
 	{
 		return 1;
 	}
 
-	if(!openFile(OUT, argv[2], "w"))
+	if(!openFile(&OUT, argv[2], "w"))
 	{
 		return 1;
 	}
 
-	if(!openFile(COMMANDS, "src/commands.yaml", "rb"))
+	if(!openFile(&COMMANDS, "src/commands.yaml", "rb"))
 	{
 		return 1;
 	}
@@ -57,6 +75,7 @@ int main(int argc, char* argv[])
 	yaml_parser_t parser;
 	yaml_document_t keyDocs;
 	yaml_node_t *node;
+	yaml_event_t event;
 
 	//starts parser
 	if(!yaml_parser_initialize(&parser))
@@ -67,25 +86,43 @@ int main(int argc, char* argv[])
 	printf("loaded parser\n");
 
 	yaml_parser_set_input_file(&parser, COMMANDS);
-	printf("set input command file");
+	printf("set input command file\n");
 
 	if(!yaml_parser_load(&parser, &keyDocs))
 	{
 		printf("faild to load parser");
 		return 1;
 	}
-	else
-	{
-		printf("loaded yaml parser\n");
-	}
+	printf("loaded yaml parser\n");
 
 
 	///////////////////////////////////////////////////////////////////done starting files
+	
+	int done = 0;
+	//reads the yaml file to set up the commands that will be translated
+	while(!done)
+	{
+		//creates the event and checks for errors
+		if(!yaml_parser_parse(&parser, &event))
+		{
+			printf("error reading commands file");
+			return 1;
+		}
 
+		//checks if the file is done
+		done = (event.type == YAML_STREAM_END_EVENT);
+
+		//frees up the event memory
+		yaml_event_delete(&event);
+	}
+
+	printf("done loading commands\n");
+	
+	//frees up the memory from the parser
+	yaml_parser_delete(&parser);
 
 	//unloads all objects from memory
 	printf("exiting\n");
-	yaml_parser_delete(&parser);
 	fclose(IN);
 	fclose(OUT);
 	fclose(COMMANDS);
